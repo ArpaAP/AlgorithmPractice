42a90ca0409ec5d8891ba688d5831a6ba2deb854
알파 <arpa1705@gmail.com>
2025-04-05 17:34:33 +0900

    update

---
+++ 5/2.py
@@ -0,0 +1,31 @@
+# 5-2
+# Binomial Coefficient: Memoization
+
+import sys
+
+sys.setrecursionlimit(10**6)
+
+N, K = map(int, input().split())
+K = min(K, N - K)
+
+dp = {}
+count = 0
+
+def binom(n, k):
+    global count
+
+    count += 1
+
+    if k == 0 or k == n:
+        return 1
+    
+    if (n, k) in dp:
+        return dp[(n, k)]
+    
+    r = (binom(n - 1, k - 1) + binom(n - 1, k)) % 10007
+    dp[(n, k)] = r
+
+    return r
+
+print(binom(N, K))
+print(count)
